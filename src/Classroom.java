6306ec219b7a66e8d592d4f1da7e2ba59ec99537
BalssemZoghbi <zoghbibalssem@gmail.com>
2023-02-01 15:21:46 +0100

    reformate classroom code

--- src/Classroom.java
+++ src/Classroom.java
@@ -16,14 +16,13 @@ public class Classroom {
     public String getClassroomName() {
         return classroomName;
     }
+
     public int getNumberStudent() {
         return numberStudent;
     }
+    
      public String toString(){
         return 
               "Hello Balssem, this is the classroom!";
      }
-
-
-    
 }
