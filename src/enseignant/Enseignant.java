f28f5f6b3888c3350ec751c9119d576375cec94c
jnayeh <mouradslim44@gmail.com>
2023-02-01 15:22:19 +0100

    adds toString to Enseignant

--- src/enseignant/Enseignant.java
+++ src/enseignant/Enseignant.java
@@ -1,7 +1,18 @@
 package enseignant;
 
 public class Enseignant {
+    private String name;
+    private String lastname;
     public static void main(String[] args) {
         System.out.println("Hello mourad");
     }
+
+    @Override
+    public String toString() {
+        return "Enseignant{" +
+                "name='" + name + '\'' +
+                ", lastname='" + lastname + '\'' +
+                '}';
+    }
+
 }
