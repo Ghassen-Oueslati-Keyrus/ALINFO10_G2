86ae5a350fcbec90a66d6543b00eeaa7cb737992
ichrak_friji <ichrak.friji@keyrus.com>
2023-02-01 14:53:08 +0100

    first commit

--- src/Etudiant.java
+++ src/Etudiant.java
@@ -4,5 +4,19 @@ public class Etudiant {
     private String email;
     private String phone;
 
+    public String getName() {
+        return name;
+    }
 
+    public String getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
 }
