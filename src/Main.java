060c9116172f766319972d1979d66acb64597c0b
ichrak_friji <ichrak.friji@keyrus.com>
2023-02-06 11:09:09 +0100

    add second  student

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello we are ALINFO10_G2");
         Etudiant etudiant1 = new Etudiant("ichrak friji","1","ichrak.friji@esprit.tn","12345678");
-        var etudiant_details = etudiant1.toString();
-        System.out.println(etudiant_details);
+        var etudiant1_details = etudiant1.toString();
+        System.out.println(etudiant1_details);
+        Etudiant etudiant2 = new Etudiant("ichrak friji","2","ichrak.friji@gmail.com","123456");
+        var etudiant2_details = etudiant2.toString();
+        System.out.println(etudiant2_details);
     }
 }
