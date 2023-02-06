d9b887f19f823aaf7904bf4107ec9afbdfbcbdcf
ichrak_friji <ichrak.friji@keyrus.com>
2023-02-06 10:31:55 +0100

    format code, fix packages.

--- src/Universite.java
+++ src/Universite.java
@@ -1,46 +1,44 @@
-import java.sql.SQLOutput;
-
 public class Universite {
-    public String id ;
+    public String id;
     public String name;
     public String adresse;
 
-    public Universite(String id , String name, String adresse){
+    public Universite(String id, String name, String adresse) {
         this.id = id;
         this.name = name;
         this.adresse = adresse;
     }
 
-     public String getId() {
+    public String getId() {
         return id;
     }
+
     public void setId(String id) {
         this.id = id;
     }
-    
-       public String getName() {
+
+    public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
-    
-         public String getAdress() {
+
+    public String getAdress() {
         return adresse;
     }
+
     public void setAdress(String adresse) {
         this.adresse = adresse;
     }
-      public Universite(){
-        
-      }      
 
-    public String toString(){
-        return
-                "Hello oumaima, this is universite !";
+    public Universite() {
+
     }
 
-
-
+    public String toString() {
+        return "Hello oumaima, this is universite !";
+    }
 
 }
