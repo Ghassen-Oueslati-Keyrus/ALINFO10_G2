99085f39b2693d281daca4c6ebfa15c9ce2773be
BalssemZoghbi <zoghbibalssem@gmail.com>
2023-02-07 20:38:16 +0100

    classroom test

--- src/Classroom.java
+++ src/Classroom.java
@@ -3,7 +3,15 @@ public class Classroom {
     public String classroomName;
     public int numberStudent;
 
-    public Classroom(int id , String classroomName, int numberStudent){
+    public Classroom() {
+    }
+    
+    public Classroom(String classroomName, int numberStudent) {
+        this.classroomName = classroomName;
+        this.numberStudent = numberStudent;
+    }
+
+    public Classroom(int id, String classroomName, int numberStudent) {
         this.id = id;
         this.classroomName = classroomName;
         this.numberStudent = numberStudent;
@@ -21,8 +29,53 @@ public class Classroom {
         return numberStudent;
     }
     
-     public String toString(){
-        return 
-              "Hello Balssem, this is the classroom!";
-     }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setClassroomName(String classroomName) {
+        this.classroomName = classroomName;
+    }
+
+    public void setNumberStudent(int numberStudent) {
+        this.numberStudent = numberStudent;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 53 * hash + this.id;
+        hash = 53 * hash + this.numberStudent;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Classroom other = (Classroom) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (this.numberStudent != other.numberStudent) {
+            return false;
+        }
+        return true;
+    }
+     
+    @Override
+    public String toString() {
+        return "Classroom{" + "id=" + id + ", classroomName=" + classroomName + ", numberStudent=" + numberStudent + '}';
+    }
+ 
+   public void afficher() {
+        System.out.println( "Hello Balssem, this is the classroom!");
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,52 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello, World!");
+        System.out.println("Hello we are ALINFO10_G2");
+        Universite U1 = new Universite(1," Université de Carthage " , "Carthage ");
+        System.out.println(U1.message());
+        System.out.println(U1.toString());
+        Universite U2 = new Universite(2," iset Rades " , "Rades ");
+        System.out.println(U2.toString());
+        System.out.println("*****************************");
+        Etudiant etudiant1 = new Etudiant("ichrak friji","1","ichrak.friji@esprit.tn","12345678");
+        String  etudiant1_details = etudiant1.toString();
+        System.out.println(etudiant1_details);
+        Etudiant etudiant2 = new Etudiant("ichrak friji","2","ichrak.friji@gmail.com","22");
+        String etudiant2_details = etudiant2.toString();
+        System.out.println(etudiant2_details);
+        System.out.println("******************************");
+        if(etudiant2.equals(etudiant1)){
+            System.out.println(" student 1 is not equal to student 2 ");
+        }else{
+            System.out.println("student 1 is equal to  student 2 ");
+        }
+        System.out.println("*****************************");
+        etudiant1.setPhone("23123678");
+        System.out.println(etudiant1);
+        etudiant2.setPhone("00034435");
+        System.out.println(etudiant2);
+        System.out.println("*****************************");
+        System.out.println("Nom:"+etudiant1.getName());
+        System.out.println("id:"+etudiant2.getId());
+
+        System.out.println("*****************************");
+        Directeur D1 = new Directeur("1","wejdene bedoui");
+        System.out.println(D1.toString());
+        Directeur D2 = new Directeur("2","wejdene bedoui");
+        System.out.println(D2.toString());
+        System.out.println("*****************************");
+
+        System.out.println("*****************************");
+        Classroom Classroom1 = new Classroom(1,"1AINFO10",201);
+        System.out.println(Classroom1.toString());
+        Classroom Classroom2 = new Classroom(2,"1AINFO11",500);
+        System.out.println(Classroom2.toString());
+        System.out.println("******************************");
+        if(Classroom2.equals(Classroom1)){
+            System.out.println(" first Classroom 1 is not equal to the second Classroom  ");
+        }else{
+            System.out.println("first Classroom 1 is equal tothe second Classroom 2 ");
+        }
+        System.out.println("*****************************");
     }
+
 }
